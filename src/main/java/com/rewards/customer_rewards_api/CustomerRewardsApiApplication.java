c1992f6fadb59703caaa680a6fe94f19cd930d0e
BAGayathri <gayathri.akshintala@gmail.com>
2026-06-11 15:13:59 +0530

    Initial project setup

---
+++ src/main/java/com/rewards/customer_rewards_api/CustomerRewardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.rewards.customer_rewards_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerRewardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerRewardsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rewards/customer_rewards_api/CustomerRewardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewards.customer_rewards_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRewardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
